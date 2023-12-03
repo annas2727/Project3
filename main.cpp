5323ac7423a90be7f8e6c58b4e57a2ef051630bd
AidanMDev1 <116731599+AidanMDev1@users.noreply.github.com>
2023-12-03 13:12:00 -0500

    Update main.cpp
    commenting out the test code

--- main.cpp
+++ main.cpp
@@ -11,22 +11,9 @@ int main() {
     // Lazy way to source the file
 
     UFOlist List("files/Project3/cmake-build-debug\\UFOPROJECTCSV.csv");
-    // Checks every grid tile
-    int w = 0;
-    for (int j = 23; j <= 54; j++) {
-        for (int i = -128; i <= -65; i++) {
-            // for each UFO sighting within grid tile
-            for (const auto& sighting : List.GetSightingsAt(j, i).second) {
-                // if valid sighting
-                if (List.GetSightingsAt(j, i).first)
-                    cout << ++w << '\n';
-                //cout << " " << sighting.coordinate.first << " ," << sighting.coordinate.second << " , " << sighting.date << ", " << sighting.shape << " , " << sighting.duration << endl;
-            }
-        }
-    }
-
+    /*
     // check to see if sorts work
-    vector<UFOsighting> og_list = List.GetSightingsAt(40, -90).second; // test num
+    vector<UFOsighting> og_list = List.GetSightingsAt(40, -90).second; // test nums
     for (const auto& sight : og_list) {
         cout << sight.date << " ";
     }
@@ -35,6 +22,22 @@ int main() {
     for (const auto& sight : og_list) {
         cout << sight.date << " ";
     }
+    */
 
+    /*
+     // Checks every grid tile
+    int w = 0;
+    for (int j = 23; j <= 54; j++) {
+        for (int i = -128; i <= -65; i++) {
+            // for each UFO sighting within grid tile
+            for (const auto& sighting : List.GetSightingsAt(j, i).second) {
+                // if valid sighting
+                if (List.GetSightingsAt(j, i).first)
+                    cout << ++w << '\n';
+                // << " " << sighting.coordinate.first << " ," << sighting.coordinate.second << " , " << sighting.date << ", " << sighting.shape << " , " << sighting.duration << endl;
+            }
+        }
+    }
+    */
     startWindow(List);
 }
