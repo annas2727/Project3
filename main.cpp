1f1d38581ab42ca2b268bd6e75f920e4b4e3c789
AidanMDev1 <116731599+AidanMDev1@users.noreply.github.com>
2023-12-04 18:20:00 -0500

    removed old debug comments from main

--- main.cpp
+++ main.cpp
@@ -1,38 +1,6 @@
 #include "Window.h"
 #include "UFO.h"
 
-
 int main() {
-    // Lazy way to source the file
-
-    /*
-    // check to see if sorts work
-    vector<UFOsighting> og_list = List.GetSightingsAt(40, -90).second; // test nums
-    for (const auto& sight : og_list) {
-        cout << sight.date << " ";
-    }
-    cout << "\n";
-    quickSort(og_list, 0, og_list.size() - 1); // can be swapped with merge sort
-    for (const auto& sight : og_list) {
-        cout << sight.date << " ";
-    }
-    */
-
-    /*
-     // Checks every grid tile
-    int w = 0;
-    for (int j = 23; j <= 54; j++) {
-        for (int i = -128; i <= -65; i++) {
-            // for each UFO sighting within grid tile
-            for (const auto& sighting : List.GetSightingsAt(j, i).second) {
-                // if valid sighting
-                if (List.GetSightingsAt(j, i).first)
-                    cout << ++w << '\n';
-                // << " " << sighting.coordinate.first << " ," << sighting.coordinate.second << " , " << sighting.date << ", " << sighting.shape << " , " << sighting.duration << endl;
-            }
-        }
-    }
-    */
-
     startWindow();
 }
